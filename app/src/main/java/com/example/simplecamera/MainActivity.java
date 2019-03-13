3799ef02eeeab3bfceda95f427dfaf9571fd1bcb
Francine Lapid <mflapid@gmail.com>
2019-03-13 18:21:24 -0500

    did camera functionality

--- app/src/main/java/com/example/simplecamera/MainActivity.java
+++ app/src/main/java/com/example/simplecamera/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.simplecamera;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageView imageView;
+    private Button takePicButton;
+    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        takePicButton = (Button) findViewById(R.id.button1);
+        takePicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //Bundle extras = data.getExtras();
+            //Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //imageView.setImageBitmap(imageBitmap);
+            Bitmap bitmap = (Bitmap) intent.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
